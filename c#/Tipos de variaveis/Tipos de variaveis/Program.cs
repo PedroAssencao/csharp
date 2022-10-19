c42a11b563beef7b9d7f50142a424a706d62f50e
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2022-10-19 14:26:31 -0300

    c# basico

---
+++ c#/Tipos de variaveis/Tipos de variaveis/Program.cs
@@ -0,0 +1,15 @@
+﻿   
+
+
+int num1 = 5;
+int num2 = num1;
+int resultado = num1 + num2;
+num1 = 6;
+
+
+Console.WriteLine("valor do num 1 é: {0} e está na stack",num1.ToString());
+Console.WriteLine("valor do num 2 é: {0} e está na stack", num2.ToString());
+//console.ReadKey();
+
+
+
