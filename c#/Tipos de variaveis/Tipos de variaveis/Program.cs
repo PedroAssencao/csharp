bfeb098ce9415f676cfcc30099b84c2c2b47d64a
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2022-10-19 14:33:02 -0300

    Att

--- c#/Tipos de variaveis/Tipos de variaveis/Program.cs
+++ c#/Tipos de variaveis/Tipos de variaveis/Program.cs
@@ -1,15 +1,32 @@
-﻿   
-
-
-int num1 = 5;
-int num2 = num1;
-int resultado = num1 + num2;
-num1 = 6;
-
-
-Console.WriteLine("valor do num 1 é: {0} e está na stack",num1.ToString());
-Console.WriteLine("valor do num 2 é: {0} e está na stack", num2.ToString());
-//console.ReadKey();
+﻿
 
 
 
+    int num1 = 5;
+    int num2 = num1;
+    int resultado = num1 + num2;
+    num1 = 6;
+
+
+    Console.WriteLine("valor do num 1 é: {0} e está na stack", num1.ToString());
+    Console.WriteLine("valor do num 2 é: {0} e está na stack", num2.ToString());
+    Console.ReadKey();
+
+
+
+void somar()
+
+
+{
+
+    int nm1 = 5;
+    int num2 = 6;
+    int resultado = nm1 + num2;
+    Console.WriteLine("O resultado é: {0} ", resultado.ToString());
+
+
+
+   
+
+
+}
