12368e1dd6af84f7053dc495a9ceefdb2821dfd3
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2022-10-19 16:24:43 -0300

    att 2

---
+++ c#/Tipos de variaveis/Tipos de variaveis/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos_de_variaveis
+{
+    internal class Calculadora
+
+    {
+
+        public void somar()
+
+
+        {
+
+            int nm1 = 5;
+            int num2 = 6;
+            int resultado = nm1 + num2;
+            Console.WriteLine("O resultado é: {0} ", resultado.ToString());
+            Console.ReadKey();
+
+            
+
+
+
+
+
+
+        }
+    }
+}
--- c#/Tipos de variaveis/Tipos de variaveis/Program.cs
+++ c#/Tipos de variaveis/Tipos de variaveis/Program.cs
@@ -2,31 +2,43 @@
 
 
 
-    int num1 = 5;
+using Tipos_de_variaveis;
+
+
+
+int num1 = 5;
     int num2 = num1;
     int resultado = num1 + num2;
     num1 = 6;
 
+ Console.WriteLine("valor do num 1 é: {0} e está na stack", num1.ToString());
+ Console.WriteLine("valor do num 2 é: {0} e está na stack", num2.ToString());
+ Console.ReadKey();
 
-    Console.WriteLine("valor do num 1 é: {0} e está na stack", num1.ToString());
-    Console.WriteLine("valor do num 2 é: {0} e está na stack", num2.ToString());
-    Console.ReadKey();
+ Console.WriteLine("Calculadora");
+ Calculadora minhaCalculadora = new Calculadora();
+ minhaCalculadora.somar();
 
+ Console.ReadKey();
 
+ Console.WriteLine("Variaveis nulla");
+int? altura = null;
 
-void somar()
-
-
+if (altura.HasValue)
 {
 
-    int nm1 = 5;
-    int num2 = 6;
-    int resultado = nm1 + num2;
-    Console.WriteLine("O resultado é: {0} ", resultado.ToString());
 
-
-
-   
+    Console.WriteLine("O valor é: {0}: ", altura);
 
 
 }
+else
+{
+
+
+    Console.WriteLine("Altura está nullo");
+
+
+}
+Console.ReadKey();
+
