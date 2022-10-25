9e502a1d93c2106ba32a55dab39a195fce2716ba
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2022-10-25 09:59:23 -0300

    Primeiro teste byte

---
+++ c#/Tipos de variaveis/TipoByteInt/Program.cs
@@ -0,0 +1,56 @@
+﻿
+byte v1 = 10;
+
+
+Console.WriteLine("");
+Console.WriteLine("O valor de V1 é: {0}",v1);
+
+Console.WriteLine("O tipo é: {0}", v1.GetType());
+
+Console.WriteLine("Digite um valor na faixa de 0 a 255");
+
+
+
+
+
+
+
+
+//var Vdigitado = Console.ReadLine();
+
+try
+{
+
+    byte vdigitado = Convert.ToByte(Console.ReadLine());
+     Console.WriteLine("O valor digitado é byte");
+
+}
+catch (OverflowException)
+{
+    Console.WriteLine("O valor digitado não é byte");
+
+}
+
+
+
+
+
+
+
+
+
+
+//if (Vdigitado <= 255) 
+//{
+  //  Console.WriteLine("O valor digitado é byte");
+//}
+//else
+//{
+ //   Console.WriteLine("O valor digitado não é byte");
+//}
+
+
+Console.WriteLine("O valor digitado é: {0}");
+Console.ReadKey();
+
+
